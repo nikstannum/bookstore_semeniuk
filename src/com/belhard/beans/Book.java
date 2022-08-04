99866677eed2201a57e42cc16aa047951ab17891
Nikita Semeniuk <nikstannum@yandex.ru>
2022-08-04 11:52:37 +0300

    Initial commit

---
+++ src/com/belhard/beans/Book.java
@@ -0,0 +1,89 @@
+package com.belhard.beans;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+public class Book {
+    private Long id;
+    private String title;
+    private String author;
+    private String isbn;
+    private Integer pages;
+    private BigDecimal price;
+
+    public Book() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public Integer getPages() {
+        return pages;
+    }
+
+    public void setPages(Integer pages) {
+        this.pages = pages;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Book)) return false;
+        Book book = (Book) o;
+        return id.equals(book.id) && isbn.equals(book.isbn);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, isbn);
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", isbn='" + isbn + '\'' +
+                ", pages=" + pages +
+                ", price=" + price +
+                '}';
+    }
+}
---
+++ src/com/belhard/dao/BookDao.java
@@ -0,0 +1,4 @@
+package com.belhard.dao;
+
+public interface BookDao {
+}
