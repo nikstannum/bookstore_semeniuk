386045e2e7f46cda397d1fc6b95ef50b9130aafa
Nikita Semeniuk <nikstannum@yandex.ru>
2022-09-08 18:15:18 +0300

    create class LoginCommand

---
+++ src/main/java/com/belhard/controller/command/impl/LoginCommand.java
@@ -0,0 +1,15 @@
+package com.belhard.controller.command.impl;
+
+import com.belhard.controller.command.Command;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+public class LoginCommand implements Command {
+
+	@Override
+	public String execute(HttpServletRequest req) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
